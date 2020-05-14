f88366dd437728c0165d3b99e127ab18a6412aef
Franco Correa <franco@francocorrea.com>
2020-05-13 23:50:01 -0300

    No deno support for now, need to find a way to unify TS imports

--- example/deno/example.ts
+++
@@ -1,8 +0,0 @@
-import { generateValidationDigit } from '../../src/index.ts';
-
-console.log('This is a Deno example for the ciuy package');
-
-console.log(
-  `generateValidationDigit('1234567') =>`,
-  generateValidationDigit('1234567')
-);
